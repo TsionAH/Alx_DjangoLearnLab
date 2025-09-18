0b8289a90c550f2a619d48033165933bc1676574
TsionAH <tsionalemuhandiso@gmail.com>
2025-09-18 14:30:36 -0700

    git commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import DetailView
 from .models import Library
-class libraryDetailView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # add app folder prefix
     context_object_name = 'library'
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
