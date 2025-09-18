6515e60c26d8e4d7cc05244044c40c3c1a67ae3e
Tsion <tsionalemuhandiso@gmail.com>
2025-09-18 10:58:23 -0700

    Delete Alx_DjangoLearnLab/Alx_DjangoLearnLab-main directory

--- Alx_DjangoLearnLab/Alx_DjangoLearnLab-main/social_media_api/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-import views
-
-
-urlpatterns = [
-    path('register/', views.RegistrationView.as_view),
-    path('login/', views.LoginView.as_view)
-]
