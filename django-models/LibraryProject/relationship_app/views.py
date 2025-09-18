210666852d82b611bee1903b344b3888cda65095
TsionAH <tsionalemuhandiso@gmail.com>
2025-09-18 14:34:15 -0700

    git commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from .models import Book, Library
-from django.views.generic.detail import DetailView
-
-
+from .models import Book
+from django.views.generic import DetailView
+from .models import Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # add app folder prefix
