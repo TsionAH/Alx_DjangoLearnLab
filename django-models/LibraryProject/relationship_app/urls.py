bfe4e935105a24ca224fe10619ec6a3fd75d5120
TsionAH <tsionalemuhandiso@gmail.com>
2025-09-20 00:17:33 -0700

    urls edited and add path of viewes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from . import views
 from .views import list_books, LibraryDetailView
 
 urlpatterns = [
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+     path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
     path('books/', list_books, name='list_books'),  # FBV URL
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # CBV URL
 ]
