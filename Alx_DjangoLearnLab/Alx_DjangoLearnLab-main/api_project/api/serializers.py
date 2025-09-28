e67e5aee283606cc4ffb1e3f8c394b35e2376fe5
TsionAH <tsionalemuhandiso@gmail.com>
2025-09-28 06:31:10 -0700

    added

--- Alx_DjangoLearnLab/Alx_DjangoLearnLab-main/api_project/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Book
-
-class BookSerializer(serializers.ModelSerializer):
-    "Serialzer for the book class"
-    class Meta:
-        model = Book
-        fields = ['title', 'author']
