8b1b67ba1137edd88c5bd9d7a4c680c44f26ba9c
TsionAH <tsionalemuhandiso@gmail.com>
2025-09-28 08:31:20 -0700

    add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 
 class BookListView(APIView):
     def get(self, request):
@@ -19,19 +21,25 @@ class AuthorListView(APIView):
         return Response(serializer.data)
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # anyone can read
+
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # anyone can read
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # only logged-in users can create
 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # only logged-in users can update
 
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    permission_classes = [IsAuthenticated] 
